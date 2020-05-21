2f9038fc1b05e4cd546162d3f73ebebc40547a65
Pranjal Garg <gargp1065@gmail.com>
2020-05-21 15:09:09 +0530

    Updates

---
+++ precache-manifest.fd54e6f658325395f78db2e3ca0708ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f1873f504134dc52ec8d69991901a2",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "fa283cfe62ced35c51dc",
+    "url": "/to-do-list/static/css/main.cd366d6c.chunk.css"
+  },
+  {
+    "revision": "2e3bab956cb454b38233",
+    "url": "/to-do-list/static/js/2.c9a49bf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.c9a49bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa283cfe62ced35c51dc",
+    "url": "/to-do-list/static/js/main.27eb0883.chunk.js"
+  },
+  {
+    "revision": "67f06f65b5850567fceb",
+    "url": "/to-do-list/static/js/runtime-main.0cdf09fe.js"
+  }
+]);
